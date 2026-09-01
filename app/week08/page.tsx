0629656722adac640d6b7cf90eea866ac028f107
nichakon <nichakon.pra@rmutto.ac.th>
2026-09-01 11:36:19 +0700

    week08

---
+++ app/week08/page.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+import Loading from "./components/Loading";
+import { ShopItem } from "./components/ShopItem";
+import ShopList from "./components/ShopList";
+
+export default function ShopHome(){
+
+    return (
+            <div className="max-w-3x1 mx-auto mt-6">
+                <h1 className="text-3xl font-bold">
+                    Shop (Shop List)
+                </h1>
+                <Suspense fallback={<Loading />}>
+                    <ShopList data={ShopItem} />
+                </Suspense>
+            </div>
+        );
+
+}
